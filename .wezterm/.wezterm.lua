15f8403b62d2fd5d862646291ca4f8c049d91410
Michael Padiernos <mike@padiernos.me>
2024-10-21 16:22:28 -0400

    [build] lots

--- .wezterm/.wezterm.lua
+++ .wezterm/.wezterm.lua
@@ -4,6 +4,8 @@ local wezterm 		= require 'wezterm'
 -- This will hold the configuration.
 local config 		= wezterm.config_builder()
 
+config.color_scheme	= 'One Dark (Gogh)'
+
 config.font			= wezterm.font('ShureTechMono Nerd Font')
 config.font_size	= 14 
 config.line_height	= 1.5
