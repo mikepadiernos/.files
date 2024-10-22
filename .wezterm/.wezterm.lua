c63e15a93c25a959352df0caa61e5fce3818cbb9
Michael Padiernos <mike@padiernos.me>
2024-10-22 10:32:46 -0400

    [build] added .tool-versions file to .asdf

--- .wezterm/.wezterm.lua
+++ .wezterm/.wezterm.lua
@@ -1,26 +1,28 @@
 -- Pull in the wezterm API
-local wezterm 		= require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This will hold the configuration.
-local config 		= wezterm.config_builder()
+local config = wezterm.config_builder()
 
-config.color_scheme	= 'One Dark (Gogh)'
+local font_default = "ShureTechMono Nerd Font"
 
-config.font			= wezterm.font('ShureTechMono Nerd Font')
-config.font_size	= 14 
-config.line_height	= 1.5
+config.color_scheme = "One Dark (Gogh)"
+
+config.font = wezterm.font(font_default)
+config.font_size = 14
+config.line_height = 1.5
 
 config.window_frame = {
-  font 				= wezterm.font { family = 'ShureTechMono Nerd Font' },
-  font_size			= 14,
-  line_height		= 1.5,
+	font = wezterm.font({ family = font_default }),
+	font_size = 14,
+	line_height = 1.5,
 }
 
 config.window_padding = {
-	left 			= 7,
-	right 			= 7,
-	top 			= 7,
-	bottom 			= 7,
+	left = 7,
+	right = 7,
+	top = 7,
+	bottom = 7,
 }
 
 return config
