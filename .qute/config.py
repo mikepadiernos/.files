ed097efd7e8f9f3f37d35a60b5aed410ad6b89e1
Michael Padiernos <mike@padiernos.me>
2025-03-19 17:45:33 -0400

    configs [build]
    - alot of updates

--- .qute/config.py
+++ .qute/config.py
@@ -1,4 +1,9 @@
+from mappings import bind
+
 config.load_autoconfig()
+
+font                            = "GohuFont 11 Nerd Font"
+home                            = "https://qutebrowser.org/"
 
 c.colors.tabs.bar.bg            = "#000000"
 c.colors.tabs.even.bg           = "#000000"
@@ -9,10 +14,12 @@ c.colors.tabs.selected.even.bg  = "#efe100"
 c.colors.tabs.selected.odd.bg   = "#efe100"
 c.colors.tabs.selected.even.fg  = "#000000"
 c.colors.tabs.selected.odd.fg   = "#000000"
-c.fonts.default_family          = "GohuFont 11 Nerd Font"
+c.fonts.default_family          = font
 c.fonts.default_size            = "12pt"
 c.tabs.position                 = "bottom"
 c.tabs.title.alignment          = "center"
-c.url.default_page              = "https://qutebrowser.org/"
-c.url.start_pages               = "https://qutebrowser.org/"
+c.url.default_page              = home
+c.url.start_pages               = home
 c.window.title_format           = "{perc}{current_title}"
+
+bind(config)
---
+++ .qute/mappings.py
@@ -0,0 +1,4 @@
+def bind(config):
+    config.bind("<Ctrl-Shift-Left>", ":tab-prev")
+    config.bind("<Ctrl-Shift-Right>", ":tab-next")
+    config.bind("<Ctrl-Shift-Del>", ":quit")
